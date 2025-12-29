b0c8a4119a5d1cb823e4038660df0dbe9c5200b5
matsuokashuhei <matsuokashuheiii@gmail.com>
2025-12-29 16:42:46 +0900

    Initial Commit

---
+++ PortalTests/PortalTests.swift
@@ -0,0 +1,17 @@
+//
+//  PortalTests.swift
+//  PortalTests
+//
+//  Created by 松岡周平 on 2025/12/29.
+//
+
+import Testing
+@testable import Portal
+
+struct PortalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
